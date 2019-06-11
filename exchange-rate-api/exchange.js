483f2ecfb68a6edc4709465abb178ba037e11798
Megha Prasad Bindiganavale <Megha@Meghas-MacBook-Pro.local>
2019-06-11 13:33:35 -0700

    change add to list style to select option dropdown form

--- exchange-rate-api/exchange.js
+++ exchange-rate-api/exchange.js
@@ -2,7 +2,7 @@
 function callApi (){
     $("#results").empty();
 
-    var endCurrency = $(this).attr("data-name");
+    var endCurrency = $(".currency-list").val();
     console.log(endCurrency);
     
     var queryURL = "https://openexchangerates.org/api/latest.json?app_id=296d12934b28477790da87204c8eae86&symbols="+endCurrency;
@@ -28,6 +28,7 @@ function callApi (){
         method: "GET"
     }).then(function(response){
         var currencies = JSON.parse(response);
+        console.log(currencies);
         //console.log(response);
         //var logo = response[endCurrency].symbol;
         
@@ -41,21 +42,58 @@ function callApi (){
     })
 }
 
-$("#addCurrencyBtn").on("click", function(event){
-    event.preventDefault();
+// $(".currency-list").on("click", function(event){
+//     event.preventDefault();
     //console.log(startCode);
-    var symbol = $("#symbols-input").val();
-    // endCode.toUpperCase();
-    console.log("end code: "+symbol);
+    // var symbol = $("#symbols-input").val();
+    // // endCode.toUpperCase();
+    // console.log("end code: "+symbol);
     
-    var newButton = $("<button>");
-    newButton.addClass("dropdown-item");
-    newButton.attr("type", "button");
-    newButton.attr("data-name", symbol)
-    console.log("data-name: "+newButton.attr("data-name"));
-    newButton.text(symbol);
-    $(".dropdown-menu").append(newButton);
+    // var newButton = $("<button>");
+    // newButton.addClass("dropdown-item");
+    // newButton.attr("type", "button");
+    // newButton.attr("data-name", symbol)
+    // console.log("data-name: "+newButton.attr("data-name"));
+    // newButton.text(symbol);
+    // $(".dropdown-menu").append(newButton);
+    $.ajax({
+        url: "https://gist.githubusercontent.com/Fluidbyte/2973986/raw/b0d1722b04b0a737aade2ce6e055263625a0b435/Common-Currency.json",
+        method: "GET"
+    }).then(function(response){
+        var currencies = JSON.parse(response);
+        console.log(currencies);
 
-})
+        var keys = Object.keys(currencies)
+        // console.log(keys)
+        for (var i=0; i<keys.length; i++){
+            var newButton = $("<option>");
+            //newButton.addClass("dropdown-item");
+            //newButton.attr("type", "button");
+            newButton.attr("value", keys[i])
+            newButton.addClass("currency-option")
+            // console.log("data-name: "+newButton.attr("data-name"));
+            newButton.text(keys[i]);
+            $(".currency-list").append(newButton);     
+        }
+        //console.log(response);
+        //var logo = response[endCurrency].symbol;
+        // for (var key in response){
+        //         console.log(key);
+                // var currencyCode = key.code;
+                // console.log("code: "+currencyCode);
+                // var newButton = $("<button>");
+                // newButton.addClass("dropdown-item");
+                // newButton.attr("type", "button");
+                // newButton.attr("data-name", currencyCode)
+                // console.log("data-name: "+newButton.attr("data-name"));
+                // newButton.text(currencyCode);
+                // $(".dropdown-menu").append(newButton);
+            
+        
+    })
+// })
 
-$(document).on("click", ".dropdown-item", callApi);
+$(document).on("click", "#addCurrencyBtn", function(event){
+    event.preventDefault()
+    callApi()
+});
