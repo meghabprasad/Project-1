7d7062d8dfc4ebac57b40b9c91b04a9fdfd835d9
Adam <lehreradam@yahoo.com>
2019-06-12 16:53:07 -0700

    fixing merge conflicts

--- flight-api.js
+++ flight-api.js
@@ -1,1 +1,79 @@
-var queryURL:("http://api.travelpayouts.com/v2/prices/latest?currency=usd&period_type=year&page=1&limit=30&show_to_affiliates=true&sorting=price&trip_class=0&token=PutHereYourToken9fce94023aebd6561cac3dc4d92c0482");
+$(document).ready(function(event){
+
+
+console.log("hi")
+$(".current").on("click",function(){
+origin =$(this).val()
+console.log(this)
+console.log(origin);
+$("#comingFrom").text(origin);
+
+})
+
+$(".going").on("click",function(){
+   destination =$(this).val()
+  console.log(this)
+  console.log(destination);
+  $("#goingTo").text(destination);
+  
+})
+
+
+})
+
+
+$("#to").on("click",function(){
+  
+  $(".graph").empty();
+
+  var departDate=$("#departDate").val()
+  console.log(departDate)
+
+  
+var queryURL="http://min-prices.aviasales.ru/calendar_preload?origin="+origin+"&destination="+ destination+"&depart_date="+ departDate +"&one_way=false&currency=usd&show_to_affiliates=false"
+
+$.ajax({
+  url:queryURL,
+  method:"GET"
+}).then(function(response){
+
+  console.log(queryURL);
+  console.log(response);
+
+  for(var i = 0; i<5;i++){
+    console.log(response.current_depart_date_prices)
+    var result=response.current_depart_date_prices
+      tr=$("<tr>");
+      // console.log(tr)
+      tr.attr("class","graph");
+      tr.append("<td>"+result[i].value.toFixed(2));
+      tr.append("<td>"+result[i].return_date+"</td>");
+      console.log(typeof(tr));
+      tr.append("<td>"+result[i].gate+"</td>");
+      tr.append("<td>"+result[i].number_of_changes+"</td>");
+      $("#flightPrices").append(tr)
+
+  console.log(result[i].return_date);
+  console.log(result[i].value)
+  console.log(result[i].gate)
+  console.log(result[i].number_of_changes)
+  
+    
+  // $("#flightPrices").append("<td class='well'>"+ result[i].value.toFixed(2)+ "</td>"+"<td class='member-name'>" +result[0].return_date + " </td>"+ "<td class='member-role'>" +result[0].gate+" </td>" + "<td class='member-startDate'>"+ result[0].number_of_changes +"</td>");
+  
+
+  
+  // $("#flightPrices").append("<tr<td class='well'>"+ result[1].value.toFixed(2)+ "</td>" +"<td class='member-name'>" +result[1].return_date + " </td>" + "<td class='member-role'>"+result[1].gate+" </td> <td class='member-startDate'>"+ result[1].number_of_changes + "</td>></tr>");
+
+
+  
+
+
+  }
+})
+
+
+
+})
+
+
