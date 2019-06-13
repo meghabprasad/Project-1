c0438deabe5f9b855f2b167bfb2e464704684c0d
Greyson Gerhard-Young <greyson_gerhard-young@brown.edu>
2019-06-12 18:37:40 -0700

    branch updated with Adam's js

--- flight-api.js
+++ flight-api.js
@@ -1,63 +1,78 @@
 $(document).ready(function(event){
 
 
-console.log("hi")
-$(".current").on("click",function(){
-origin =$(this).val()
-console.log(this)
-console.log(origin);
-$("#comingFrom").text(origin);
-
-})
-
-$(".going").on("click",function(){
-   destination =$(this).val()
+  console.log("hi")
+  $(".current").on("click",function(){
+  origin =$(this).val()
   console.log(this)
-  console.log(destination);
-  $("#goingTo").text(destination);
+  console.log(origin);
+  $("#comingFrom").text(origin);
   
-})
-
-
-
-
-$("#to").on("click",function(){
-
-  var departDate=$("#departDate").val()
-  console.log(departDate)
-
+  })
   
-var queryURL="http://min-prices.aviasales.ru/calendar_preload?origin="+origin+"&destination="+ destination+"&depart_date="+ departDate +"&one_way=false&currency=usd&show_to_affiliates=false"
-
-$.ajax({
-  url:queryURL,
-  method:"GET"
-}).then(function(response){
-
-  
-
-  for(var i = 0; i<5;i++){
-    console.log(response.current_depart_date_prices)
-    var result=response.current_depart_date_prices
+  $(".going").on("click",function(){
+     destination =$(this).val()
+    console.log(this)
+    console.log(destination);
+    $("#goingTo").text(destination);
     
-
-  console.log(result[i].return_date);
-  console.log(result[i].value)
-  console.log(result[i].gate)
-  console.log(result[i].number_of_changes)
+  })
+  
+  
+  })
+  
+  
+  
+  $("#to").on("click",function(){
+    
+    $(".graph").empty();
+  
+    var departDate=$("#departDate").val()
+    console.log(departDate)
   
     
-  $("#flightPrices").append("<th class='well'>"+ result[0].value.toFixed(2) +"<th class='member-name'>" +result[0].return_date + " </th><th class='member-role'>" + result[0].gate + "</th><th class='member-startDate'>"+ result[0].number_of_changes + "</th><th class='member-rate'></th>");
+  var queryURL="http://min-prices.aviasales.ru/calendar_preload?origin="+origin+"&destination="+ destination+"&depart_date="+ departDate +"&one_way=false&currency=usd&show_to_affiliates=false"
   
-  $("#flightPrices").append("<tr class='well'>"+ result[0].value.toFixed(2) +"<td class='member-name'>" +result[0].return_date + " </td><td class='member-role'>" + result[0].gate + "</td><td class='member-startDate'>"+ result[0].number_of_changes + "</td><td class='member-rate'></td>");
-
-
-  }
-
-})
-
-
-
-})
-
-})
+  $.ajax({
+    url:queryURL,
+    method:"GET"
+  }).then(function(response){
+  
+    console.log(queryURL);
+    console.log(response);
+  
+    for(var i = 0; i<5;i++){
+      console.log(response.current_depart_date_prices)
+      var result=response.current_depart_date_prices
+        tr=$("<tr>");
+        // console.log(tr)
+        tr.attr("class","graph");
+        tr.append("<td>"+result[i].value.toFixed(2));
+        tr.append("<td>"+result[i].return_date+"</td>");
+        console.log(typeof(tr));
+        tr.append("<td>"+result[i].gate+"</td>");
+        tr.append("<td>"+result[i].number_of_changes+"</td>");
+        $("#flightPrices").append(tr)
+  
+    console.log(result[i].return_date);
+    console.log(result[i].value)
+    console.log(result[i].gate)
+    console.log(result[i].number_of_changes)
+    
+      
+    // $("#flightPrices").append("<td class='well'>"+ result[i].value.toFixed(2)+ "</td>"+"<td class='member-name'>" +result[0].return_date + " </td>"+ "<td class='member-role'>" +result[0].gate+" </td>" + "<td class='member-startDate'>"+ result[0].number_of_changes +"</td>");
+    
+  
+    
+    // $("#flightPrices").append("<tr<td class='well'>"+ result[1].value.toFixed(2)+ "</td>" +"<td class='member-name'>" +result[1].return_date + " </td>" + "<td class='member-role'>"+result[1].gate+" </td> <td class='member-startDate'>"+ result[1].number_of_changes + "</td>></tr>");
+  
+  
+    
+  
+  
+    }
+  })
+  
+  
+  
+  })
